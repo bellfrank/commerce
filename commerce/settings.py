c5b828ebb730442b63d8f9e41edeed44206a00cc
bellfrank <franciscocw0101@gmail.com>
2022-03-09 14:16:45 -0800

    added a postgres database

--- commerce/settings.py
+++ commerce/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbcfiagt9kvaq8',
+        'USER': 'ocgmemygkyshlf',
+        'PASSWORD': 'ccbf50fcfd2e0f5bd8b1cb6088cc05121a8d42bbfd16d05fc0a8848580160dfb',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
